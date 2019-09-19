d2aaf3cd8e234bc711703ff29b5d2ec7104d8cf0
Shaun Orpen <shaunorpen@users.noreply.github.com>
2019-09-19 13:02:59 +0100

    Working on Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -16,6 +16,32 @@ function isSubmitting () {
     return false;
 }
 
+function renderForm (props) {
+    return ( 
+        <Form>
+            <div>
+                <label htmlFor='name'>Name: </label>
+                <Field type="text" name="name" />
+            </div>
+            <div>
+                <label htmlFor='email'>Email: </label>
+                <Field type="email" name="email" />
+            </div>
+            <div>
+                <label htmlFor='Password'>Password: </label>
+                <Field type="password" name="password" />
+            </div>
+            <div>
+                <Field type="checkbox" name="tos" />
+                <label htmlFor='tos'>I agree to the Terms of Service</label>
+            </div>
+            <button type="submit" disabled={isSubmitting}>
+                Submit
+            </button>
+        </Form>
+    );
+}
+
 function UserForm () {
     return (
         <div>
@@ -23,19 +49,7 @@ function UserForm () {
             <Formik 
                 initialValues={initialValues}
                 onSubmit={onSubmit}
-                render={ props => {
-                    return ( 
-                        <Form>
-                            <Field type="text" name="name" />
-                            <Field type="email" name="email" />
-                            <Field type="password" name="password" />
-                            <Field type="checkbox" name="tos" />
-                            <button type="submit" disabled={isSubmitting}>
-                                Submit
-                            </button>
-                        </Form>
-                    )
-                }}
+                render={renderForm}
             />
         </div>
     );
