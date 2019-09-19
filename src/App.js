2ad830d84044ddbe9211e72326b75a069946115c
Shaun Orpen <shaunorpen@users.noreply.github.com>
2019-09-19 12:54:17 +0100

    Add form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import Form from './components/Form';
+import UserForm from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <UserForm />
     </div>
   );
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,44 @@
 import React from 'react';
+import { Formik, Form, Field } from 'formik';
 
-function Form () {
+const initialValues = {
+    name: '',
+    email: '',
+    password: '',
+    tos: '',
+}
+
+function onSubmit () {
+    return null;
+}
+
+function isSubmitting () {
+    return false;
+}
+
+function UserForm () {
     return (
         <div>
-            
+            <h1>Add New User</h1>
+            <Formik 
+                initialValues={initialValues}
+                onSubmit={onSubmit}
+                render={ props => {
+                    return ( 
+                        <Form>
+                            <Field type="text" name="name" />
+                            <Field type="email" name="email" />
+                            <Field type="password" name="password" />
+                            <Field type="checkbox" name="tos" />
+                            <button type="submit" disabled={isSubmitting}>
+                                Submit
+                            </button>
+                        </Form>
+                    )
+                }}
+            />
         </div>
     );
 }
 
-export default Form;
+export default UserForm;
